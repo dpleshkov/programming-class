ed7a47439f5c10d0209be39e6059319711c189dd
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2019-10-03 21:47:35 -0700

    Added new file containining how to deal with errors

--- week3/functions.py
+++ week3/functions.py
@@ -1,5 +1,9 @@
 # Example of defining functions of python and using return statements
 
+def hello_world(): # define function hello_world
+    print("Hello world")
+
+hello_world()
 
 def multiply(x, y): # define function multiply that takes parameter x and parameter y
     return x * y # return the parameter x multiplied by parameter y
@@ -8,9 +12,3 @@ def multiply(x, y): # define function multiply that takes parameter x and parame
 print(2 * 2)
 
 print(multiply(2, 2))
-
-
-def hello_world(): # define function hello_world
-    print("Hello world")
-
-hello_world()
