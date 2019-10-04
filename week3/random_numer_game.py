c3c6be7c753f40cf324eabae94c858fc5b40b753
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2019-10-04 12:27:07 -0700

    Got rid of try-except blocks

--- week3/random_numer_game.py
+++ week3/random_numer_game.py
@@ -4,32 +4,84 @@ import random
 #Selects random number and establishes how many guesses you have
 number = random.choice(range(0, 100))
 guesses_left = 10
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 #Prompts the user to guess a number - if they don't choose an actual number the computer will make them redo it
-try:
-    print("\n{} guesses left".format(guesses_left))
-    guess = int(input("Guess a number (between 1 and 100): "))
-except ValueError:
-    guess = int(input("Not a number, please try again: "))
+print("\n{} guesses left".format(guesses_left))
+guess = input("Guess a number (between 1 and 100): ")
+is_not_number = False
+for letter in guess:
+    if letter not in numbers:
+        is_not_number = True
+        break
+if is_not_number == False:
+    guess = int(guess)
+else:
+    while is_not_number == True:
+        guess = input("Not a number, try again: ")
+        is_not_number2 = False
+        for letter in guess:
+            if letter not in numbers:
+                is_not_number2 = True
+                break
+        if is_not_number2 == False:
+            is_not_number = False
+    
+if is_not_number == False:
+    guess = int(guess)
 
 #Game loop - continues until the game ends
 while guess != number:
-
+    
     if guess > number:
-        try:
-            guesses_left -= 1
-            print("\n{} guesses left".format(guesses_left))
-            guess = int(input("Number is smaller, try again: "))
-        except ValueError:
-            guess = int(input("Not a number, please try again: "))
+        guesses_left -= 1
+        print("\n{} guesses left".format(guesses_left))
+        guess = input("Number is smaller, try again: ")
+        for letter in guess:
+            if letter not in numbers:
+                is_not_number = True
+                break
+        if is_not_number == False:
+            guess = int(guess)
+        else:
+            while is_not_number == True:
+                guess = input("Not a number, try again: ")
+                is_not_number2 = False
+                for letter in guess:
+                    if letter not in numbers:
+                        is_not_number2 = True
+                        break
+                if is_not_number2 == False:
+                    is_not_number = False
+    
+        if is_not_number == False:
+            guess = int(guess)
+
             
     if guess < number:
-        try:
-            guesses_left -= 1
-            print("\n{} guesses left".format(guesses_left))
-            guess = int(input("Number is larger, try again: "))
-        except ValueError:
-            guess = int(input("Not a number, please try again: "))
+        guesses_left -= 1
+        print("\n{} guesses left".format(guesses_left))
+
+        guess = input("Number is larger, try again: ")
+        for letter in guess:
+            if letter not in numbers:
+                is_not_number = True
+                break
+        if is_not_number == False:
+            guess = int(guess)
+        else:
+            while is_not_number == True:
+                guess = input("Not a number, try again: ")
+                is_not_number2 = False
+                for letter in guess:
+                    if letter not in numbers:
+                        is_not_number2 = True
+                        break
+                if is_not_number2 == False:
+                    is_not_number = False
+    
+        if is_not_number == False:
+            guess = int(guess)
 
     #If you run out of guesses, you lose
     if guesses_left == 0:
